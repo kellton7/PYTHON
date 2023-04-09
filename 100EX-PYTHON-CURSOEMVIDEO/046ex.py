5ff973a405c6b0406a4fa8896cb6077f53f0ee61
Kelton Lima <kelton491@gmail.com>
2023-04-09 20:09:35 -0300

    CONTAGEM DE PARES
    EXERCÍCIO 046 PYTHON

---
+++ 100EX-PYTHON-CURSOEMVIDEO/046ex.py
@@ -0,0 +1,12 @@
+#Programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
+# indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+from time import sleep
+
+print(' ')
+print('  INICIANDO A CONTAGEM REGRESSIVA ')
+print(' ')
+
+for c in range(10, 0, -1):
+    print( c), sleep(1)
+print('FIM DA CONTAGEM')
