608acb81bee997fc9d3397922b9cf4e6c4ab4e01
Kelton Lima <kelton491@gmail.com>
2023-04-09 19:29:35 -0300

    DOBRO, TRIBLO E RAIZ QUADRADA
    EXERCÍCIO 006 PYTHON

---
+++ 100EX-PYTHON-CURSOEMVIDEO/006ex.py
@@ -0,0 +1,7 @@
+#CRIE UM ALGORITMO QUE LEIA UM NUMERO E MOSTRE O SEU DOBRO, TRIBLO E RAIZ QUADRADA
+
+n = int(input('Digite um numero: '))
+print(f'O dobro desse numero é: {n*2}, o seu triblo é: {n*3}, e sua raiz quadrada é: {n**(1/2):.2f}')
+
+
+
