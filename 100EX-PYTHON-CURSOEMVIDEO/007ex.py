872cb5f1bf4a1b382b7f215c7efecddd3f6d2013
Kelton Lima <kelton491@gmail.com>
2023-04-09 19:30:14 -0300

    MÉDIA
    EXERCÍCIO 007 PYTHON

---
+++ 100EX-PYTHON-CURSOEMVIDEO/007ex.py
@@ -0,0 +1,8 @@
+#DESENVOLVA UM PROGRAMA QUE LEIA AS DUAS NOTAS DE UM ALUNO, CALCULE E MOSTRE SUA MEDIA
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+media = (n1 + n2) / 2
+
+print(f'A media entre {n1} e {n2} é: {media:.2f}')
+
