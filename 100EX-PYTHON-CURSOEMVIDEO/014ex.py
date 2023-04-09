0b56d00793a58592a04fe09fa200c507f439c8be
Kelton Lima <kelton491@gmail.com>
2023-04-09 19:35:51 -0300

    TEMPERATURA EM ºC CONVERTIDA PARA ªF
    EXERCÍCIO 014 PYTHON

---
+++ 100EX-PYTHON-CURSOEMVIDEO/014ex.py
@@ -0,0 +1,7 @@
+#Escreva um programa que converta uma temperatura digitada em ºC e converta para ªF
+
+c = float(input('Digite a temperatuda em ºC: '))
+f = (c * 1.8) + 32
+print(f'A temperatura em ºC {c:.1f} convertida para ºF {f:.1f}')
+
+
