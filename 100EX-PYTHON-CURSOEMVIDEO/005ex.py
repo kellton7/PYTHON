87418acda02e1b5cfa593e0a231cf8d5441ea86e
Kelton Lima <kelton491@gmail.com>
2023-04-09 19:28:56 -0300

    SUCESSOR E ANTECESSOR
    EXERCÍCIO OO5 PYTHON

---
+++ 100EX-PYTHON-CURSOEMVIDEO/005ex.py
@@ -0,0 +1,9 @@
+
+#FAÇA UM PROGRAMA QUE LEIA UM NUMERO INTEIRO E MOSTRE NA TELA O SEU SUCESSOR E ANTECESSOR
+
+n = int(input('Digite um valor inteiro: '))
+print(f'O proximo numero depois de {n} é: {n+1}', end=' ')
+print(f'e o numero anterior a ele é: {n-1}')
+
+
+
