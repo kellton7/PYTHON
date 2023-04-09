8ffdccaba2f06c37817c1a448196c6ca5dd7f34e
Kelton Lima <kelton491@gmail.com>
2023-04-09 19:24:18 -0300

    SOMA
    Exercício 003 PYTHON

---
+++ 100EX-PYTHON-CURSOEMVIDEO/003ex.py
@@ -0,0 +1,7 @@
+#CRIE UM PROGRAMA QUE LEIA DOIS NUMEROS E MOSTRE A SOMA ENTRE ELES
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
+soma = n1 + n2
+print(f'A soma de {n1} + {n2} e igual a: {soma}')
+
